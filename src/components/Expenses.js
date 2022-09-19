abb746fb4b1adf49f3e4278cd407ff6c7c46e94a
Oksana <ksenia79460@gmail.com>
2022-09-19 19:13:37 +0100

    display items dynamically

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,29 +1,26 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+
 import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const filterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterHandler} />
+
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
